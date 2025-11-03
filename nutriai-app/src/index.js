9b196c089e96412a55683e5a648f211d2f10dc0f
imenbenrejeb298 <i.benrejeb.hb@gmail.com>
2025-11-03 21:16:15 +0100

    Fix: Remise de index.html dans public et suppression de web-vitals

--- nutriai-app/src/index.js
+++ nutriai-app/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals'; // This line is commented out
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -14,21 +14,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
-
-// Register service worker for PWA (if supported)
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker
-      .register('/sw.js')
-      .then((registration) => {
-        // Registration successful
-        // eslint-disable-next-line no-console
-        console.log('ServiceWorker registration successful with scope: ', registration.scope);
-      })
-      .catch((err) => {
-        // eslint-disable-next-line no-console
-        console.warn('ServiceWorker registration failed: ', err);
-      });
-  });
-}
+// reportWebVitals(); // This line is also commented out
